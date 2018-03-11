33bdbcaee0731c6f298e55a9d79847076d7c1214
Olexandr Kordunyan <kordunyanolexandr@gmail.com>
2018-03-11 19:51:22 +0200

    Start to create domain layer

---
+++ src/main/java/com/kordunyan/NhsystemApplication.java
@@ -0,0 +1,12 @@
+package com.kordunyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhsystemApplication.class, args);
+	}
+}
---
+++ src/main/java/com/kordunyan/domain/User.java
@@ -0,0 +1,61 @@
+package com.kordunyan.domain;
+
+import java.util.List;
+
+public class User {
+
+	private String email;
+	private String name;
+	private String password;
+	private List<Task> tasks;
+	private List<Role> roles;
+
+	public User() {
+	}
+
+	public User(String email, String name, String password) {
+		this.email = email;
+		this.name = name;
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+}
